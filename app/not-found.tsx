dc1f4d51c0f6aaa1cc6e708296bf0ce984d9f94e
VoskanGrigoryan98 <voskan.grigoryan.arg@gmail.com>
2023-10-10 23:38:09 -0300

    finally finishing stuff

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,30 +1,30 @@
-// "use client";
+"use client";
 
-// import { useRouter } from "next/navigation";
-// //redirect from next/navigate not working for some reason
-// // import { Result } from "antd";
-// import CButton from "@/components/ui/Button";
+import { useRouter } from "next/navigation";
+//redirect from next/navigate not working for some reason
+import { Result } from "antd";
+import CButton from "@/components/ui/Button";
 
-// export default function Custom404() {
-//   const router = useRouter();
+export default function Custom404() {
+  const router = useRouter();
 
-//   return (
-//     <div>
-//       {/* <Result
-//         status="404"
-//         title="404"
-//         subTitle="Sorry, the page you visited does not exist."
-//         extra={
-//           <CButton
-//             onClick={() => {
-//               router.push("/home");
-//             }}
-//             style={{ backgroundColor: "#4096ff" }}
-//           >
-//             Back Home
-//           </CButton>
-//         }
-//       /> */}
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <CButton
+            onClick={() => {
+              router.push("/home");
+            }}
+            style={{ backgroundColor: "#4096ff" }}
+          >
+            Back Home
+          </CButton>
+        }
+      />
+    </div>
+  );
+}
