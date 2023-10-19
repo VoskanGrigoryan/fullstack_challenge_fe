22c8f2a5dd5701b83633eb49b4b06e8fcd8f8e9e
VoskanGrigoryan98 <voskan.grigoryan.arg@gmail.com>
2023-10-19 09:16:31 -0300

    adding missing project file to views

--- components/views/Register.tsx
+++ components/views/Register.tsx
@@ -1,47 +1,49 @@
-/* eslint-disable react/no-unescaped-entities */
 "use client";
 
-import Link from "next/link";
-import { useState } from "react";
-import { useForm, SubmitHandler, Controller } from "react-hook-form";
-import { useRouter } from "next/navigation";
-import { KeyOutlined, UserOutlined } from "@ant-design/icons";
-import { Input, Typography } from "antd";
+import AuthContainer from "@/components/containers/AuthContainer";
 import CButton from "@/components/ui/Button";
+import { KeyOutlined, MailOutlined, UserOutlined } from "@ant-design/icons";
+import { Input, Typography } from "antd";
+import Link from "next/link";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
 type IFormInputs = {
+  email: string;
   username: string;
   password: string;
+  confirmPassword: string;
 };
+
+interface FormInputs {
+  singleErrorInput: string;
+}
 
 const schema = yup
   .object({
+    email: yup.string().email().required(),
     username: yup.string().required(),
     password: yup.string().required(),
+    confirmPassword: yup
+      .string()
+      .required("confirm password is required")
+      .oneOf([yup.ref("password")], "Passwords must match"),
   })
   .required();
 
 const { Text } = Typography;
 
-export default function Login() {
-  const router = useRouter();
-
-  const [userData, setUserData] = useState<IFormInputs>();
-
+export default function Register() {
   const {
     handleSubmit,
     control,
+    watch,
     formState: { errors },
   } = useForm<IFormInputs>({
     resolver: yupResolver(schema),
   });
-
-  const onSubmit: SubmitHandler<IFormInputs> = (data) => {
-    setUserData(data), router.push("/dashboard");
-  };
-
+  const onSubmit: SubmitHandler<IFormInputs> = (data) => {};
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
@@ -51,6 +53,28 @@ export default function Login() {
         flexDirection: "column",
       }}
     >
+      <Controller
+        name="email"
+        control={control}
+        rules={{
+          required: "This is not a valid email",
+          pattern: /^\S+@\S+$/i,
+        }}
+        render={({ field }) => (
+          <Input
+            status={errors.email?.message ? "error" : ""}
+            {...field}
+            placeholder="Email"
+            style={{ marginTop: "12px" }}
+            prefix={<MailOutlined />}
+          />
+        )}
+      />
+
+      <p style={{ padding: 0, margin: 0, color: "red" }}>
+        {errors.email?.message}
+      </p>
+
       <Controller
         name="username"
         control={control}
@@ -78,7 +102,7 @@ export default function Login() {
           <Input.Password
             {...field}
             status={errors.password?.message ? "error" : ""}
-            placeholder="input password"
+            placeholder="Password"
             style={{ marginTop: "12px" }}
             prefix={<KeyOutlined />}
           />
@@ -89,13 +113,32 @@ export default function Login() {
         {errors.password?.message}
       </p>
 
+      <Controller
+        name="confirmPassword"
+        control={control}
+        rules={{ required: true }}
+        render={({ field }) => (
+          <Input.Password
+            {...field}
+            status={errors.confirmPassword?.message ? "error" : ""}
+            placeholder="Confirm password"
+            style={{ marginTop: "12px" }}
+            prefix={<KeyOutlined />}
+          />
+        )}
+      />
+      <p style={{ padding: 0, margin: 0, color: "red" }}>
+        {errors.confirmPassword?.message}
+      </p>
+
       <div style={{ marginTop: 20 }}>
         <CButton htmlType="submit" type="primary">
-          Login
+          Register
         </CButton>
       </div>
+
       <Text style={{ marginTop: "12px", textAlign: "center" }}>
-        Don't have an account yet? <Link href="/auth/register">Register</Link>
+        Already have an account? <Link href="/auth/login">Login</Link>
       </Text>
     </form>
   );
